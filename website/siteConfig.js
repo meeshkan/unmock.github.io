e987c74fd3faf2227a09dce06d79910dc7bc7c87
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-07-22 14:28:31 +0300

    Fix the project name.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,7 +21,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: "unmock-docs",
+  projectName: "unmock.github.io",
   organizationName: "unmock",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
