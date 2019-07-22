ae0beeee81551d1e01d0fa458d28d040b7fd72be
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-07-22 17:03:15 +0300

    Use docs as the landing page (#1)
    * Clean up comments.
    
    * Use empty docs url.
    
    * Redirect to docs introduction.
    
    * Do not include html in redirect site.
    
    * Add empty index.js.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,17 +15,11 @@ const siteConfig = {
   title: "Unmock", // Title for your website.
   tagline: "Make mocking fun again",
   url: "https://unmock.github.io", // Your website URL
-  baseUrl: "/", // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
-
+  baseUrl: "/", // Base URL for your project
+  docsUrl: "", // Defaults to `docs`
   // Used for publishing and more
   projectName: "unmock.github.io",
   organizationName: "unmock",
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [{ doc: "introduction", label: "Docs" }],
