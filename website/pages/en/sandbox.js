98018b453b358362153abb70fabcee626338f6c8
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-07-23 15:02:43 +0300

    Embed an example with Jest tests.

--- website/pages/en/sandbox.js
+++ website/pages/en/sandbox.js
@@ -4,10 +4,9 @@ const React = require("react");
 // https://docusaurus.io/docs/en/api-pages#page-require-paths
 const CompLibrary = require("../../core/CompLibrary.js");
 
-const iframe = `<iframe src="https://codesandbox.io/embed/node?fontsize=14" title="node" allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media" style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;" sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>`;
+const iframe = `<iframe src="https://codesandbox.io/embed/n9m2w9q8x0?fontsize=14" title="Jest test" allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media" style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;" sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>`;
 
 const Container = CompLibrary.Container;
-const MarkdownBlock = CompLibrary.MarkdownBlock;
 
 class Sandbox extends React.Component {
   render() {
@@ -17,7 +16,6 @@ class Sandbox extends React.Component {
     return (
       <div className="mainContainer">
         <Container padding={["bottom", "top"]}>
-          <MarkdownBlock># Play with unmock!</MarkdownBlock>
           <div dangerouslySetInnerHTML={{ __html: iframe }} />
         </Container>
       </div>
