7f2a59e2d55a95906355ccdd3f1dc0f64a4ec611
Mike Solomon <mike@meeshkan.com>
2019-09-12 09:18:45 +0300

    Adds gitter

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -74,7 +74,13 @@ const siteConfig = {
 
   // Add custom scripts here that would be placed in <script> tags.
   scripts: [
-    "//katacoda.com/embed.js"
+    "//katacoda.com/embed.js",
+    "https://unpkg.com/unmock-gitter@0.0.0/index.js",
+    {
+      src: "https://sidecar.gitter.im/dist/sidecar.v1.js",
+      async: true,
+      defer: true
+    }
   ],
 
   // On page navigation for the current documentation page.
