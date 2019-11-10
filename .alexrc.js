051c029f4c0a0ea84d30c1b00212ac0dba1b37d9
Carolyn Stransky <carolynstransky@gmail.com>
2019-11-10 18:48:49 +0100

    Changed noBinary default to true

--- .alexrc.js
+++ .alexrc.js
@@ -5,3 +5,6 @@ exports.allow = [
   
 // Use a "maybe" level of profanity instead of the default "unlikely"
 exports.profanitySureness = 1;
+
+// Changing default to catch all instances of gendered language
+exports.noBinary = true;
